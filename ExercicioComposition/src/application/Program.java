a1de5a814b6bebbcb9f463140d1eb72407e3963f
Ronald Freitas <ronaldfreitasds@gmail.com>
2023-06-10 00:22:04 -0300

    Salvamento

---
+++ ExercicioComposition/src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Date date = new Date();
+		
+		String moment = sc.nextLine();
+		sdf.parse(moment);
+		sc.close();
+	}
+
+}
