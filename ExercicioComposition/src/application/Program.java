83d78a14dbf58478923b11ffd5e04da196374908
Ronald Freitas <ronaldfreitasds@gmail.com>
2023-06-11 19:00:32 -0300

    Atualizando

--- ExercicioComposition/src/application/Program.java
+++ ExercicioComposition/src/application/Program.java
@@ -6,6 +6,12 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
@@ -13,9 +19,48 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Date date = new Date();
+		Order ord = new Order();
 		
-		String moment = sc.nextLine();
-		sdf.parse(moment);
+		String data = sdf.format(date);
+		Date moment = sdf.parse(data);
+		
+		System.out.println("Enter client data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.nextLine());
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		String status = sc.nextLine();
+		
+		Order order = new Order(moment, OrderStatus.valueOf(status), client);
+		
+		System.out.print("How many itens to this order? ");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter #" + i + " item data: ");
+			System.out.print("Product name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			Double price = sc.nextDouble();
+			System.out.print("Quantity: ");
+			Integer quantity = sc.nextInt();
+			Product product = new Product(productName, price);
+			OrderItem it = new OrderItem(quantity, price, product);
+			order.addOrderItem(it);
+			
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMMARY: ");
+		System.out.println(order);
+		
+		
 		sc.close();
 	}
 
