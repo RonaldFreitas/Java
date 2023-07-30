ffaa203a057b2fbd3ebaf8e35460e8cbbad114ef
Ronald Freitas <ronaldfreitasds@gmail.com>
2023-07-30 01:21:13 -0300

    Atualização

--- ExcecaoPersonalizada/src/model/entities/Reservation.java
+++ ExcecaoPersonalizada/src/model/entities/Reservation.java
@@ -16,7 +16,7 @@ public class Reservation {
 	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		if(!checkOut.after(checkIn)) {
-			throw new DomainException("Error in reservation: Check-out date must be after check-in date. ");
+			throw new DomainException("Check-out date must be after check-in date. ");
 		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -47,10 +47,10 @@ public class Reservation {
 	public void updateDates(Date checkIn, Date checkOut) {
 		Date now = new Date();
 		if(checkIn.before(now) || checkOut.before(now)) {
-			throw new DomainException("Error in reservation: reservation dates must be future dates. ");
+			throw new DomainException("Reservation dates must be future dates. ");
 		} 
 		if(!checkOut.after(checkIn)) {
-			throw new DomainException("Error in reservation: Check-out date must be after check-in date. ");
+			throw new DomainException("Check-out date must be after check-in date. ");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
