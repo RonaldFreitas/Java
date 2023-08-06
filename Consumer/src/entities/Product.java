92717d4205ace78dbe1c6ff36366868c6e1ba345
Ronald Freitas <ronaldfreitasds@gmail.com>
2023-08-05 21:27:41 -0300

    Consumer reference method(non-static method)

--- Consumer/src/entities/Product.java
+++ Consumer/src/entities/Product.java
@@ -31,6 +31,11 @@ public class Product {
 
 	}
 	
+	public void nonStaticPriceUpdate() {
+		price = price * 1.1;
+
+	}
+	
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
